837262331a352e3e363f53bd36d3951f1f9ed5ef
couldwhite <75570282+couldwhite@users.noreply.github.com>
2021-11-22 21:37:37 +0400

    Add spring project

---
+++ key-skill-back/src/main/java/com/training/keyskillback/KeySkillBackApplication.java
@@ -0,0 +1,13 @@
+package com.training.keyskillback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeySkillBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeySkillBackApplication.class, args);
+	}
+
+}
---
+++ key-skill-back/src/test/java/com/training/keyskillback/KeySkillBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.keyskillback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeySkillBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
